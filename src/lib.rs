999e00ed0fd032ce5d6c4fe1f453a40099400d05
nazgull08 <nazgull08@protonmail.com>
2023-05-14 07:38:10 +0300

    cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,44 @@
-mod wallet;
-mod hash;
+pub mod wallet;
 
+#[cfg(test)]
 mod tests {
-    use ethkey::EthAccount;
-    use log::info;
+    use crate::wallet::HDSeed;
+    use crate::wallet::HDWallet;
+    use bip39::{Language, Mnemonic};
 
     #[test]
     fn test_wallet() {
-        let a = EthAccount::load_or_generate("test", "test").unwrap();
-        let addr = a.address();
-        let pubkey = a.public();
-        assert_eq!("test2","test2");
-        assert_eq!("test1","test1");
+        let eth_addrs = vec![
+            "0xa03eF5A8A00b938886b5e54b228759Ce8cBb6bF5",
+            "0x94A902854842c4A5931AB49E558690b1dfd16394",
+            "0x7c132f9840602B086b51aEAa6367518b210C69D7",
+        ];
+        let tron_addrs = vec![
+            "TDLSuRq683BHiBuV9oSBFTxxJs1U1YuT1n",
+            "THh8xUn3R8B51U6UD8YAp9mhUkcRM8fJEL",
+            "TDe2FTTE6B16LzDUKLoWQvvF4iEEdYHCnM",
+        ];
+
+        let mnemonic = Mnemonic::from_phrase(
+            "debris resemble coil soul shrimp slender deal aunt twenty gown fee test",
+            Language::English,
+        )
+        .unwrap();
+        let hdw_eth = HDWallet::Ethereum(HDSeed {
+            mnemonic: mnemonic.clone(),
+        });
+        let hdw_tron = HDWallet::Tron(HDSeed { mnemonic });
+        for i in 0..3 {
+            let eth_i = hdw_eth.address(i as i32);
+            let tron_i = hdw_tron.address(i as i32);
+            println!("=======================");
+            println!("ETH");
+            println!("addr: {:?}", eth_i);
+            println!("TRON");
+            println!("addr: {:?}", tron_i);
+            assert_eq!(eth_addrs[i], eth_i);
+            assert_eq!(tron_addrs[i], tron_i);
+            println!("=======================");
+        }
     }
 }
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,21 +1,18 @@
 use std::str::FromStr;
 
-use log::info;
-
-use bip39::{Language, Mnemonic, MnemonicType, Seed};
+use ::sha256::digest;
+use bip39::{Mnemonic, Seed};
 use bitcoin::base58;
 use bitcoin::bip32::ExtendedPubKey;
 use bitcoin::{
-    network::constants::Network,
     bip32::{DerivationPath, ExtendedPrivKey},
+    network::constants::Network,
     PublicKey,
 };
-use eth_keystore::encrypt_key;
-use secp256k1::{Secp256k1};
+use secp256k1::Secp256k1;
 use serde::{Deserialize, Serialize};
-use ::sha256::digest;
+use sha3::{Digest, Keccak256};
 
-use crate::hash::keccak_hash;
 
 pub enum HDWallet {
     Ethereum(HDSeed),
@@ -27,10 +24,10 @@ pub struct HDSeed {
 }
 //NTD add funcion for save key to file
 impl HDWallet {
-    pub fn address(&self,index: i32) -> String{ 
-        match self{
-            HDWallet::Ethereum(seed) => eth_address_by_index(seed,index),
-            HDWallet::Tron(seed) => tron_address_by_index(seed,index),
+    pub fn address(&self, index: i32) -> String {
+        match self {
+            HDWallet::Ethereum(seed) => eth_address_by_index(seed, index),
+            HDWallet::Tron(seed) => tron_address_by_index(seed, index),
         }
     }
 }
@@ -61,7 +58,6 @@ impl EthAddr {
         }
         //checksum and return
         let checksummed_addr = eth_checksum::checksum(&proper_addr);
-        println!("New eth addr: {}", checksummed_addr);
         Self(checksummed_addr)
     }
     pub fn get(&self) -> &str {
@@ -69,29 +65,30 @@ impl EthAddr {
     }
 }
 
-fn eth_address_by_index(seed: &HDSeed, index : i32) -> String{
+fn eth_address_by_index(seed: &HDSeed, index: i32) -> String {
     let hd_path_str = format!("m/44'/60'/0'/0/{index}");
-    let seed_m = Seed::new(&seed.mnemonic,"");
-    let seed_bytes = seed_m.as_bytes();
-    let (_pk, pubk) = get_extended_keypair(&seed_bytes.clone(), &DerivationPath::from_str(&hd_path_str).unwrap());
+    let seed_m = Seed::new(&seed.mnemonic, "");
+    let (_pk, pubk) = get_extended_keypair(
+        seed_m.as_bytes(),
+        &DerivationPath::from_str(&hd_path_str).unwrap(),
+    );
     let eth_addr = extended_pubk_to_addr(&pubk);
-    format!("{:?}",eth_addr)
+    eth_addr.get().to_owned()
 }
 
-fn tron_address_by_index(seed: &HDSeed, index : i32) -> String{
+fn tron_address_by_index(seed: &HDSeed, index: i32) -> String {
     let hd_path_str = format!("m/44'/195'/0'/0/{index}");
-    let seed_m = Seed::new(&seed.mnemonic,"");
-    let seed_bytes = seed_m.as_bytes();
-    let (_pk, pubk) = get_extended_keypair(&seed_bytes, &DerivationPath::from_str(&hd_path_str).unwrap());
-    let tron_addr= extended_pubk_to_addr_tron(&pubk);
-    tron_addr 
+    let seed_m = Seed::new(&seed.mnemonic, "");
+    let (_pk, pubk) = get_extended_keypair(
+        seed_m.as_bytes(),
+        &DerivationPath::from_str(&hd_path_str).unwrap(),
+    );
+    extended_pubk_to_addr_tron(&pubk)
 }
-
-
 
 fn get_extended_keypair(
     seed: &[u8],
-    hd_path: &DerivationPath
+    hd_path: &DerivationPath,
 ) -> (ExtendedPrivKey, ExtendedPubKey) {
     let secp = Secp256k1::new();
     let pk = ExtendedPrivKey::new_master(Network::Bitcoin, seed)
@@ -99,11 +96,6 @@ fn get_extended_keypair(
         .and_then(|k| k.derive_priv(&secp, hd_path))
         .unwrap();
     let pubk = ExtendedPubKey::from_priv(&secp, &pk);
-
-    let a = pk;
-    let b:[u8; 32] = a.private_key.secret_bytes();
-    let pub_b = pubk.public_key.serialize_uncompressed();
-
     (pk, pubk)
 }
 
@@ -132,27 +124,27 @@ fn extended_pubk_to_addr_tron(pubk: &ExtendedPubKey) -> String {
     let pubk_bytes = hex::decode(pubk_uncomp).unwrap();
     let k_addr = &keccak_hash(&pubk_bytes);
     //keep last 20 bytes of the result
-    let experimental_addr = "41".to_owned()+&k_addr[24..];
-    let hex_exp_addr = hex::decode(&experimental_addr).unwrap(); 
+    let experimental_addr = "41".to_owned() + &k_addr[24..];
+    let hex_exp_addr = hex::decode(&experimental_addr).unwrap();
     let s_hex_exp_addr = hex_exp_addr.as_slice();
     //let exp_k_addr_b = hex::de(&experimental_addr).unwrap();
     let val0 = digest(s_hex_exp_addr);
-    let hex_val0 = hex::decode(&val0).unwrap(); 
+    let hex_val0 = hex::decode(val0).unwrap();
     let s_hex_val0 = hex_val0.as_slice();
-    let val1 = digest(s_hex_val0); 
+    let val1 = digest(s_hex_val0);
     let check_sum_val1 = &val1[0..8];
     let final_addr = experimental_addr + check_sum_val1;
     let final_addr_bytes = hex::decode(final_addr).unwrap();
 
-    let b58_res = base58::encode(&final_addr_bytes);
-    //massage into domain unit
-    b58_res
+    base58::encode(&final_addr_bytes)
 }
 
-
-pub fn tron_pub_key_from_priv_key_bytes(priv_key_bytes : [u8;32]) -> [u8;32]{
-   priv_key_bytes 
+fn keccak_hash<T>(data: &T) -> String
+where
+    T: ?Sized + Serialize + AsRef<[u8]>,
+{
+    let mut hasher = Keccak256::new();
+    hasher.update(data);
+    let result = hasher.finalize();
+    hex::encode(result)
 }
-
-
-
